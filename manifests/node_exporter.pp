88854581d71ac72ad6b240b8e44191714bbe3b89
Maarten Beeckmans <maartenb@inuits.eu>
2020-09-19 23:42:09 +0200

    remove deprecated feature collectors

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -12,13 +12,11 @@
 #
 class profile_prometheus::node_exporter (
   String $version                 = '1.0.1',
-  Array $collectors               = ['diskstats','filesystem','loadavg','meminfo','netdev','stat','tcpstat','time','vmstat'],
   Boolean $manage_firewall_entry  = true,
 )
 {
   class { 'prometheus::node_exporter':
     version    => $version,
-    collectors => $collectors,
   }
   if $manage_firewall_entry {
     ::profile_base::firewall::entry { '200 allow node_exporter':
