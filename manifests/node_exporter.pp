b1ee1a9c313285a512d485a8568c596dfbd150a2
Maarten Beeckmans <maartenb@inuits.eu>
2020-10-31 21:04:41 +0100

    add extra options linux

--- manifests/node_exporter.pp
+++ manifests/node_exporter.pp
@@ -11,12 +11,16 @@
 # $manage_firewall_entry  Manage firewall entry
 #
 class profile_prometheus::node_exporter (
-  String $version                 = '1.0.1',
-  Boolean $manage_firewall_entry  = true,
+  String         $version                = '1.0.1',
+  Boolean        $manage_firewall_entry  = true,
+  Arrays[String] $collectors_enable      = [],
+  Arrays[String] $collectors_disable     = ['buddyinfo', 'interrupts', 'logind', 'mountstats', 'ntp', 'processes', 'systemd', 'tcpstat',],
 )
 {
   class { 'prometheus::node_exporter':
-    version    => $version,
+    version            => $version,
+    collectors_enable  => $collectors_enable,
+    collectors_disable => $collectors_disable,
   }
   if $manage_firewall_entry {
     firewall { '09100 allow node_exporter':
